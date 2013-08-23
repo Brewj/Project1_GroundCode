424da0f7539c9e7138976ca7b6b32226115c2ab2
Matthew <matty.beckett@btinternet.com>
2013-08-23 07:14:27 +0100

    Update dltrigger.py

--- dltrigger.py
+++ dltrigger.py
@@ -1,25 +1,49 @@
 import time
-import sys
- 
-text_file_path = "" # location path of your text file
+import requests
+
+text_file_path = ""
 initialised = False
-the_trigger = "" # the trigger message coming from the payload
-trigger_url = "" # URL to be loaded
- 
+the_trigger = "altitude 30000m reached! I'm in space!"
+trigger_word_1 = "altitude"
+trigger_word_2 = "space"
+trigger_word_3 = "reached"
+
+altitude_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%22c3d528cd4dcc690f7c7d5e7a014ba642%22]&endkey=[%22c3d528cd4dcc690f7c7d5e7a014ba642%22,[]]&fields=altitude"
+
+def check_trigger():
+    while True:
+        with open(text_file_path, "r") as f:
+        
+            for line in f: # for each line in text file
+        
+                if str(the_trigger) in line and initialised == False: # if the line contains what is required and it hasn't already...
+                    initialised = True
+                    print "Yes! in line!"
+                    requests.post("http://requestb.in/16t4rn21")
+                    break
+                elif str(trigger_word_1) in line and initialised == False or str(trigger_word_2) in line and initialised == False or str(trigger_word_3) in line and initialised == False:
+                    initialised = True
+                    print "Yes! in line!"
+                    requests.post("http://requestb.in/16t4rn21")
+                    break 
+                else:
+                    print "not yet"
+
+def get_altitude():
+    r = requests.get(altitude_json_link) # get altitude from URL
+    altitude = float(r.json()[-1]["altitude"]) # parse altitude
+    return altitude
+    
+
 while True:
+    altitude = get_altitude() # check altitude
+    if altitude >= 25000 and altitude_reached == False: # if alt above 25km, we know it's safe to check for message
+        check_trigger() # check for message
+        altitude_reached = True
  
-    with open(text_file_path, "r") as f: # open text file
-        
-        for line in f: # for each line in text file
-        
-            if str(the_trigger) in line and initialised == False: # if the line contains what is required and it hasn't already...
-                initialised = True
-                requests.get(trigger_url)
-                print "Yes! Trigger in line"
-                break
-            else: # if not in line
-                print "not yet" 
-    time.sleep(1) # delay to keep while loop stable
+    time.sleep(1)
+    print altitude
  
-    if initialised == True: # if trigger has been executed
-        break # end program
+    if initialised == True:
+        f.close()
+        break
