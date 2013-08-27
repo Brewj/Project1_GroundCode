88c316b59f4cf0eb3c1ac6c678e170585f737d9d
Matthew <matty.beckett@btinternet.com>
2013-08-27 14:40:39 +0100

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -2,11 +2,11 @@ import requests
 import json
 import urllib2
 
-trigger_url = ""
- 
-triggered = False 
-altitude_json_link = "" # altitude link from Habitat
-trigger_json_link = "" # trigger link from Habitat
+bet_url = "http://google.co.uk"
+print "checking altitude is more than 130m...\n"
+triggered = False # boolean to prevent duplicating bet
+altitude_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%2274658495479e1c99632d9bbf6b16f459%22]&endkey=[%2274658495479e1c99632d9bbf6b16f459%22,[]]&fields=altitude" # altitude link from Habitat
+trigger_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%2274658495479e1c99632d9bbf6b16f459%22]&endkey=[%2274658495479e1c99632d9bbf6b16f459%22,[]]&fields=trigger" # trigger link from Habitat
  
 while True: # forever
  
@@ -18,9 +18,12 @@ while True: # forever
     trigger = str(r.json()[-1]["trigger"]) # parse trigger
     print "The trigger is: " + trigger # print trigger
  
-    if altitude >= 29800 and trigger == "True" and triggered == False: # if altitude = 30000m and altitude hasn't already been reached
-        response = urllib2.urlopen(trigger_url)
+    if altitude >= 140 and trigger == "True" and triggered == False: # if altitude = 30000m and altitude hasn't already been reached
+        response = urllib2.urlopen(bet_url)
         print response.info()
+
+
+        
         print "Bet Placed! Exiting..."
         triggered = True
         break
