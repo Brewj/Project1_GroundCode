d1388e4d99faeb5a37b4a4dfdbf66f97f583ab17
Matthew <matty.beckett@btinternet.com>
2013-09-02 13:35:33 +0100

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -3,10 +3,10 @@ import json
 import urllib2
 
 bet_url = "http://google.co.uk"
-print "checking altitude is more than 130m...\n"
+print "checking altitude is more than 29500m...\n"
 triggered = False # boolean to prevent duplicating bet
-altitude_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%2274658495479e1c99632d9bbf6b16f459%22]&endkey=[%2274658495479e1c99632d9bbf6b16f459%22,[]]&fields=altitude" # altitude link from Habitat
-trigger_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%2274658495479e1c99632d9bbf6b16f459%22]&endkey=[%2274658495479e1c99632d9bbf6b16f459%22,[]]&fields=trigger" # trigger link from Habitat
+altitude_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%227830c4b51e021511a9bb8ff5dc1df89a%22]&endkey=[%227830c4b51e021511a9bb8ff5dc1df89a%22,[]]&fields=altitude" # altitude link from Habitat
+trigger_json_link = "http://habitat.habhub.org/habitat/_design/ept/_list/json/payload_telemetry/payload_time?include_docs=true&startkey=[%227830c4b51e021511a9bb8ff5dc1df89a%22]&endkey=[%227830c4b51e021511a9bb8ff5dc1df89a%22,[]]&fields=trigger" # trigger link from Habitat
  
 while True: # forever
  
@@ -18,7 +18,7 @@ while True: # forever
     trigger = str(r.json()[-1]["trigger"]) # parse trigger
     print "The trigger is: " + trigger # print trigger
  
-    if altitude >= 140 and trigger == "True" and triggered == False: # if altitude = 30000m and altitude hasn't already been reached
+    if altitude >= 29500 and trigger == "True" and triggered == False: # if altitude = 29500m and altitude hasn't already been reached
         response = urllib2.urlopen(bet_url)
         print response.info()
 
